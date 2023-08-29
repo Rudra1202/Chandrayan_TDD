20634a166e779554517ec72c5f3829250599e169
Rudra <102217181+Rudra1202@users.noreply.github.com>
2023-08-29 23:21:59 +0530

    second_TDD

--- src/tests/SourceRepo.test.js
+++ src/tests/SourceRepo.test.js
@@ -89,3 +89,78 @@ test("multiple commands", () => {
   expect(output).toStrictEqual(final);
 });
 
+//Test 7
+test("multiple commands", () => {
+  const commands = ["f", "r", "r", "l", "u"];
+  const initial = {
+    coordinates: [2, -2, 1],
+    direction: "S",
+  };
+  const final = {
+    coordinates: [2, -3, 1],
+    direction: "U",
+  };
+  const output = SourceRepo(commands, initial);
+  expect(output).toStrictEqual(final);
+});
+
+//Test 8
+test("multiple commands", () => {
+  const commands = ["f", "r", "d"];
+  const initial = {
+    coordinates: [0, 0, 0],
+    direction: "N",
+  };
+  const final = {
+    coordinates: [0, 1, 0],
+    direction: "D",
+  };
+  const output = SourceRepo(commands, initial);
+  expect(output).toStrictEqual(final);
+});
+
+//Test 9
+test("multiple commands", () => {
+  const commands = ["d", "l", "b", "b"];
+  const initial = {
+    coordinates: [0, 0, 0],
+    direction: "W",
+  };
+  const final = {
+    coordinates: [-2, 0, 0],
+    direction: "E",
+  };
+  const output = SourceRepo(commands, initial);
+  expect(output).toStrictEqual(final);
+});
+
+//Test 10
+test("multiple commands", () => {
+  const commands = ["r", "r", "r"];
+  const initial = {
+    coordinates: [1, 1, 1],
+    direction: "N",
+  };
+  const final = {
+    coordinates: [1, 1, 1],
+    direction: "W",
+  };
+  const output = SourceRepo(commands, initial);
+  expect(output).toStrictEqual(final);
+});
+
+//Test 11
+test("for multiple commands", () => {
+  const commands = ["u", "u", "f", "l"];
+  const initial = {
+    coordinates: [0, 0, 0],
+    direction: "U",
+  };
+  const final = {
+    coordinates: [0, -1, 0],
+    direction: "E",
+  };
+  const output = SourceRepo(commands, initial);
+  expect(output).toStrictEqual(final);
+});
+
